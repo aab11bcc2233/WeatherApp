898ab7d638443654c32f900451dbef1ab41e280c
hetianpeng <t.hcc@foxmail.com>
2017-10-15 22:23:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tianching/weatherapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tianching.weatherapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+
+
+    }
+
+}
